7b21843ae2f685466eb80a59188a1fdd39b9bed7
HatcherCheung <1109079622@qq.com>
2021-07-06 22:59:02 +0800

    写了shop的一部分功能

---
+++ 前端实训/京东商城/src/components/shopInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shop">
+    <img class="shop__img" :src="item.imgUrl" alt="" />
+    <div class="shop__content">
+      <div
+        :class="{
+          shop__content: true,
+          'shop__content--bordered': hideBorder ? false : true
+        }"
+      ></div>
+      <div class="content__title">{{ item.title }}</div>
+      <div class="content__tags">
+        <span class="tags__tag">月售:{{ item.sales }}</span>
+        <span class="tags__tag">起送:{{ item.expressLimit }}</span>
+        <span class="tags__tag">基础运费:{{ item.expressPrice }}</span>
+      </div>
+      <p class="content__discount">{{ item.slogan }}</p>
+    </div>
+  </div>
+</template>
+<style lang="sass" scoped>
+.shop {
+    display:flex;
+    padding-top: .12rem;
+}
+</style>
---
+++ 前端实训/京东商城/src/views/shop/Shop.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+<script>
+export default {
+  setup () {}
+}
+</script>
+<style scoped lang="scss">
+.wrapper {
+  padding: 0 0.18rem;
+}
+</style>
