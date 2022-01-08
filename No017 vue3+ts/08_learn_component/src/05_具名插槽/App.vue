12dcd2913ee6c83e1d21ef2874526f9a552ab5c4
HatcherCheung <1109079622@qq.com>
2022-01-08 19:44:36 +0800

    插槽和具名插槽的简单使用

---
+++ No017 vue3+ts/08_learn_component/src/05_具名插槽/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <MySlotCpnVue :slotName="slotName">
+      <!-- 使用v-slot来指明里面的内容需要放到哪个插槽中 -->
+      <template v-slot:one>
+        <button>第一个插槽</button>
+      </template>
+      <!-- 简写 -->
+      <template #two>
+        <button>第二个插槽</button>
+      </template>
+      <!-- 动态插槽 -->
+      <template #[slotName]>
+        <button>第三个插槽</button>
+      </template>
+    </MySlotCpnVue>
+  </div>
+</template>
+
+<script>
+import MySlotCpnVue from './MySlotCpn.vue';
+export default {
+  components: {
+    MySlotCpnVue,
+  },
+  data() {
+    return {
+      slotName: 'hat',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ No017 vue3+ts/08_learn_component/src/05_具名插槽/MySlotCpn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 定义插槽的名字 -->
+    <slot name="one"></slot>
+    <slot name="two"></slot>
+    <slot :name="slotName"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slotName: String,
+  },
+};
+</script>
+
+<style scoped></style>
