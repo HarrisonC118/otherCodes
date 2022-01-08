8af2878bc17b6a92b172d0fa6846ded525337949
HatcherCheung <1109079622@qq.com>
2022-01-09 07:38:03 +0800

    更新目录结构

--- No017 vue3+ts/08_learn_component/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './06_作用域插槽/App.vue';
-import router from './router';
-import store from './store';
-
-createApp(App).use(store).use(router).mount('#app');
---
+++ No017 vue3+ts/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './06_作用域插槽/App.vue';
+
+createApp(App).mount('#app');
