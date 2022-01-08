709788923bd44189c1ae279c07bc3f01733b54e9
HatcherCheung <1109079622@qq.com>
2022-01-08 08:35:05 +0800

    provide引用data中的数据

--- No017 vue3+ts/08_learn_component/src/03_Provide和inject/App.vue
+++ No017 vue3+ts/08_learn_component/src/03_Provide和inject/App.vue
@@ -7,14 +7,24 @@
 <script>
 import AppSub from './App_sub.vue';
 export default {
-  provide: {
-    HomeName: 'hatcher',
+  // 普通写法
+  //   provide: {
+  //     HomeName: 'hatcher',
+  //   },
+  
+  // 函数写法，可以通过this调用data中的数据
+  provide() {
+    return {
+      HomeName: this.myname,
+    };
   },
   components: {
     AppSub,
   },
   data() {
-    return {};
+    return {
+      myname: 'hatcher',
+    };
   },
 };
 </script>
