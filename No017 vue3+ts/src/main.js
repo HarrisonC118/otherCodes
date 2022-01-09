bba153ccbcf10758747e6aa4655faa0a5b08e086
HatcherCheung <1109079622@qq.com>
2022-01-09 10:33:08 +0800

    异步组件

--- No017 vue3+ts/src/main.js
+++ No017 vue3+ts/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue';
-import App from './08_动态组件的使用/App.vue';
-
+import App from './09_异步组件/App.vue';
+// 使用import函数，就可以让webpack在打包的时候分成多个包打包
+import('./09_异步组件/utils/math.js').then((res) => {
+  console.log(res.sum(1, 2));
+});
 createApp(App).mount('#app');
