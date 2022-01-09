80d14d3a6b3dcff48f21e008c6d2bcc1ff2c5886
HatcherCheung <1109079622@qq.com>
2022-01-09 08:46:20 +0800

    动态组件

---
+++ No017 vue3+ts/src/08_动态组件的使用/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button v-for="item in componentsList" :key="item" @click="changeCurrentComponentName(item)">
+      display {{ item }}
+    </button>
+    <!-- is属性可以规定到底使用哪个组件，里面的内容就是下面定义的components中的内容 -->
+    <component :is="currentComponentName" name="hatcher" :age="18"></component>
+  </div>
+</template>
+
+<script>
+import Component_1 from './Component1.vue';
+import Component_2 from './Component2.vue';
+export default {
+  components: { Component_1, Component_2 },
+  data() {
+    return {
+      componentsList: ['Component_1', 'Component_2'],
+      currentComponentName: '',
+    };
+  },
+  methods: {
+    changeCurrentComponentName(item) {
+      this.currentComponentName = item;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- No017 vue3+ts/src/main.js
+++ No017 vue3+ts/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue';
-import App from './06_作用域插槽/App.vue';
+import App from './08_动态组件的使用/App.vue';
 
 createApp(App).mount('#app');
