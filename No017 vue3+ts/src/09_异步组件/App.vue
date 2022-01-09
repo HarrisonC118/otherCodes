04b4d53c055f40e02a6e8c4a3a42856509e18cbc
HatcherCheung <1109079622@qq.com>
2022-01-09 15:24:02 +0800

    Suspense

--- No017 vue3+ts/src/09_异步组件/App.vue
+++ No017 vue3+ts/src/09_异步组件/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <Suspense>
+      <template v-slot:default>
+        <Component1></Component1>
+      </template>
+      <template v-slot:fallback> 加载失败 </template>
+    </Suspense>
+  </div>
 </template>
 
 <script>
-export default {};
+import { defineAsyncComponent } from 'vue';
+const Component1 = defineAsyncComponent(() => import('./Component1.vue'));
+export default {
+  components: {
+    Component1,
+  },
+};
 </script>
 
 <style scoped></style>
