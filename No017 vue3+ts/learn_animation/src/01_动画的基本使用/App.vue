81f405b30d42b4f99e46b607f0556d19230d10da
HatcherCheung <1109079622@qq.com>
2022-01-10 10:29:17 +0800

    动画的基本使用

---
+++ No017 vue3+ts/learn_animation/src/01_动画的基本使用/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">隐藏\显示</button>
+    <transition name="hatcher">
+      <h2 v-show="isShow">Hello World!</h2>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.hatcher-enter-active {
+  animation: changeColor 1s ease;
+}
+.hatcher-leave-active {
+  animation: changeColor 1s ease reverse;
+}
+@keyframes changeColor {
+  0% {
+    color: aqua;
+  }
+  50% {
+    color: coral;
+  }
+  100% {
+    color: cornflowerblue;
+  }
+}
+
+// .hatcher-enter-from,
+// .hatcher-leave-to {
+//   opacity: 0;
+// }
+// .hatcher-enter-to,
+// .hatcher-leaver-from {
+//   opacity: 1;
+// }
+// .hatcher-enter-active,
+// .hatcher-leave-active {
+//   transition: opacity 0.5s ease;
+// }
+</style>
