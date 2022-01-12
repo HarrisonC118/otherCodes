9be4dadb168cf1eb26bed25a68915e17fc999bcf
HatcherCheung <1109079622@qq.com>
2022-01-12 20:30:12 +0800

    获取dom元素

---
+++ No017 vue3+ts/learn_composition_api/src/02_响应式数据/DomElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span ref="hello"></span>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const hello = ref(null);
+    return { hello };
+  },
+};
+</script>
