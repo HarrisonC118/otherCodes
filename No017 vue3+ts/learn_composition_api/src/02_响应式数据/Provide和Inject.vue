8442857f7a7a93b5f4229630e5c8cf881c4e8621
HatcherCheung <1109079622@qq.com>
2022-01-12 21:38:51 +0800

    Provide和Inject

---
+++ No017 vue3+ts/learn_composition_api/src/02_响应式数据/Provide和Inject.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { provide, inject, ref, readonly } from "vue";
+export default {
+  setup() {
+    const name = ref("hatcher");
+    // 一般是不允许子组件直接修改值的，所以推荐加上readonly
+    provide("name", readonly(name.value));
+    
+    // 从provide提取出需要的值
+    const injectName = inject("name");
+    return { name, injectName };
+  },
+};
+</script>
+
+<style lang="scss"></style>
