7bdeec751a963f4f1571e0e950488fd453dec525
HatcherCheung <1109079622@qq.com>
2022-01-12 17:25:04 +0800

    watchEffect的使用

---
+++ No017 vue3+ts/learn_composition_api/src/02_响应式数据/WatchEffect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { ref, watchEffect } from "vue";
+export default {
+  setup() {
+    const name = ref("hatcher");
+    const age = ref(22);
+    // 不用指定监听的属性
+    watchEffect(() => {
+      // 用到了name就监听name,没用到的age就不监听
+      console.log("hatcher->", name.value);
+    });
+    return {
+      name,
+      age,
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
