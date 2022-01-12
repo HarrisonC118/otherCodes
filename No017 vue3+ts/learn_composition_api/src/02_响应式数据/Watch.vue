02caff1260960c4b7607569a49dedaa52694f335
HatcherCheung <1109079622@qq.com>
2022-01-12 20:52:50 +0800

    watch

---
+++ No017 vue3+ts/learn_composition_api/src/02_响应式数据/Watch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span ref="hello"></span>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, watch } from "vue";
+export default {
+  setup() {
+    // 对普通的ref对象进行监听
+    const hello = ref("hello");
+    watch(hello, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+    // 对reactive对象中的属性进行监听
+    const stu = reactive({
+      name: "hatcher",
+      age: 18,
+    });
+    watch(
+      () => {
+        return { ...stu };
+      },
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      }
+    );
+    return { hello };
+  },
+};
+</script>
--- No017 vue3+ts/learn_composition_api/src/main.js
+++ No017 vue3+ts/learn_composition_api/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from "vue";
-import App from "./02_响应式数据/ToRefs.vue";
+import App from "./02_响应式数据/DomElement.vue";
 
 createApp(App).mount("#app");
