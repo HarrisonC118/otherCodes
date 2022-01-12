9bb32257c9552b838e0925f44193648cc644a68b
HatcherCheung <1109079622@qq.com>
2022-01-12 08:35:26 +0800

    setup的基本使用方法

---
+++ No017 vue3+ts/learn_composition_api/src/01_compositionAPI/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HomeVue message="hatcher" mes2="testMessage" @sendMessage="testMethod">
+      <template #hatcher>
+        <h1>哈哈哈哈</h1>
+      </template>
+    </HomeVue>
+  </div>
+</template>
+
+<script>
+import HomeVue from "./Home.vue";
+export default {
+  name: "App",
+  components: { HomeVue },
+  methods: {
+    testMethod(message) {
+      console.log("父组件方法接收" + message + "成功哦！");
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ No017 vue3+ts/learn_composition_api/src/01_compositionAPI/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot name="hatcher"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: ["message"],
+  setup(props, { attrs, slots, emit }) {
+    console.log(props.message);
+    console.log(attrs.mes2);
+    console.log(slots);
+    emit("sendMessage", "hello");
+  },
+};
+</script>
+
+<style lang="scss"></style>
