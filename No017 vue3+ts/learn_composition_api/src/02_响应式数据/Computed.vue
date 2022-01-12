a55b41fa244dbd79ccd881a17cebb9fddcf47f07
HatcherCheung <1109079622@qq.com>
2022-01-12 17:20:32 +0800

    计算属性

---
+++ No017 vue3+ts/learn_composition_api/src/02_响应式数据/Computed.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  setup() {
+    // 直接传入一个箭头函数
+    const getAll = computed(() => "hello");
+    // 传入对象，重写getter和settter
+    const getAllB = computed({
+      get: () => "hello",
+      set(newValue) {
+        console.log(newValue);
+      },
+    });
+    // 更改计算属性的值
+    getAllB.value = "wawawa";
+    return {
+      getAll,
+      getAllB,
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
