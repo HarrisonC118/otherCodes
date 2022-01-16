4cdfb3b29093e9a5c6d5e7269ec21ebae98d68d9
HatcherCheung <1109079622@qq.com>
2022-01-16 15:55:25 +0800

    封装hooks

--- No017 vue3+ts/learn_vuex/src/App.vue
+++ No017 vue3+ts/learn_vuex/src/App.vue
@@ -4,26 +4,12 @@
 </template>
 
 <script>
-import { useStore, mapState } from "vuex";
-import { computed } from "vue";
+import { useState } from "./hooks/useState";
+
 export default {
   setup() {
-    const store = useStore();
-    // mapState返回了一个对象，里面都是computed可以用的函数
-    const mapStateValue = mapState(["name", "age"]);
-    // 创建一个空对象，用来存放处理后的数据
-    const storeValue = {};
-    // 把mapStateValue对象变成数组后遍历
-    Object.keys(mapStateValue).forEach((key) => {
-      // 函数里面会访问this.$store，但是这里没有绑定this，所以自己指定一个$store
-      const fn = mapStateValue[key].bind({
-        $store: store,
-      });
-      storeValue[key] = computed(fn);
-    });
-    return {
-      ...storeValue,
-    };
+    const storeState = useState(["name", "age"]);
+    return { ...storeState };
   },
 };
 </script>
