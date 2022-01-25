3c23544671580ed9b234aa9e6b9ce082cedc1230
HatcherCheung <1109079622@qq.com>
2022-01-25 19:18:21 +0800

    完成项目的基本框架

--- No022 cms_vue_ts/vue3-ts-cms/src/App.vue
+++ No022 cms_vue_ts/vue3-ts-cms/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
--- No022 cms_vue_ts/vue3-ts-cms/src/router/index.ts
+++ No022 cms_vue_ts/vue3-ts-cms/src/router/index.ts
@@ -1,26 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
-    path: "/",
+    path: "/login",
     name: "Home",
-    component: Home,
+    component: () => import("../views/Login.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/main",
+    name: "Main",
+    component: () => import("../views/Main.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
